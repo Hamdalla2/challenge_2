1f82a320b4b822e37c47b658040362b63a6ce413
hamdalla2 <hamdalla2@yahoo.com>
2020-11-03 20:05:52 +0200

    bye bye challenge2

--- client/up.js
+++ client/up.js
@@ -2,6 +2,6 @@ let up=(upload)=>{
 const reader = new FileReader();
 reader.addEventListener('load', (event) => {
     document.getElementById('text1').value=event.target.result;
-});
+})
 reader.readAsText(upload.value)
 }
